e396783c0b94a5dfd2eac62f1998400a516b2c1f
Jw-Bluayer <wjddn0728@naver.com>
2017-07-14 16:49:13 +0900

    conversation list

--- app/controllers/conversation_controller.rb
+++ app/controllers/conversation_controller.rb
@@ -1,2 +1,24 @@
 class ConversationController < ApplicationController
+
+    def create
+        @conversation = Conversation.get(current_user.id, params[:user_id])
+
+        add_to_conversations unless conversated?
+
+        respond_to do |format|
+          format.js
+      end
+    end
+
+    private
+
+    def add_to_conversations
+        session[:conversations] ||= []
+        session[:conversations] << @conversation.id
+    end
+
+    def conversated?
+        session[:conversations].include?(@conversation.id)
+    end
+
 end
