949797e17c17c794ac8460e571d8cccd0ab79ce8
Jw-Bluayer <wjddn0728@naver.com>
2017-07-14 16:35:00 +0900

    adding first view

---
+++ db/migrate/20170714023123_create_conversations.rb
@@ -0,0 +1,11 @@
+class CreateConversations < ActiveRecord::Migration[5.1]
+  def change
+    create_table :conversations do |t|
+      t.integer :recipient_id
+      t.integer :sender_id
+
+      t.timestamps
+    end
+  add_index :conversations, [:recipient_id, :sender_id], unique: true
+  end
+end
