d4cc84eec3681e7db94cddfdd261e11aa98bfeb7
Jw-Bluayer <wjddn0728@naver.com>
2017-07-14 17:02:30 +0900

    conversation open, close, minimize(should check the minimize)

--- app/controllers/conversation_controller.rb
+++ app/controllers/conversation_controller.rb
@@ -1,24 +1,34 @@
 class ConversationController < ApplicationController
 
     def create
-        @conversation = Conversation.get(current_user.id, params[:user_id])
+      @conversation = Conversation.get(current_user.id, params[:user_id])
 
-        add_to_conversations unless conversated?
+      add_to_conversations unless conversated?
 
-        respond_to do |format|
-          format.js
+      respond_to do |format|
+        format.js
+      end
+    end
+
+    def close
+      @conversation = Conversation.find(params[:id])
+
+      session[:conversations].delete(@conversation.id)
+
+      respond_to do |format|
+        format.js
       end
     end
 
     private
 
     def add_to_conversations
-        session[:conversations] ||= []
-        session[:conversations] << @conversation.id
+      session[:conversations] ||= []
+      session[:conversations] << @conversation.id
     end
 
     def conversated?
-        session[:conversations].include?(@conversation.id)
+      session[:conversations].include?(@conversation.id)
     end
 
 end
