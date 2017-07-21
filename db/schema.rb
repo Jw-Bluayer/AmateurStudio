6c371cb1b9594409a155d16047eac196009fac0d
홍동우 <dongwoo@hongdong-uui-MacBook-Pro.local>
2017-07-21 13:12:44 +0900

    profile scaffold랑 registration변경, 마이페이지

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170714024319) do
+ActiveRecord::Schema.define(version: 20170720150718) do
 
   create_table "conversations", force: :cascade do |t|
     t.integer "recipient_id"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20170714024319) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["recipient_id", "sender_id"], name: "index_conversations_on_recipient_id_and_sender_id", unique: true
+    t.index ["recipient_id"], name: "index_conversations_on_recipient_id"
+    t.index ["sender_id"], name: "index_conversations_on_sender_id"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -30,9 +32,27 @@ ActiveRecord::Schema.define(version: 20170714024319) do
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", default: "", null: false
+    t.string "place", default: "", null: false
+    t.text "introduction", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
