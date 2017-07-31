996f29f8578c62f957fa5700db0a0d00f31a9d87
Jw-Bluayer <wjddn0728@naver.com>
2017-07-31 17:07:30 +0900

    Friendship

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,47 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 password = 'pass123'
-1.upto(5) do |i|
   User.create(
-    email: "user-#{i}@example.com",
+    email: "user-1@example.com",
     password: password,
-    password_confirmation: password
+    password_confirmation: password,
+    name: "김덕구",
+    place: "인천",
+    introduction: "하이~~~"
   )
-end
+
+  User.create(
+    email: "user-2@example.com",
+    password: password,
+    password_confirmation: password,
+    name: "김철수",
+    place: "경기도",
+    introduction: "우쮸쮸~~~"
+  )
+
+  User.create(
+    email: "user-3@example.com",
+    password: password,
+    password_confirmation: password,
+    name: "한정민",
+    place: "분당",
+    introduction: "늬늬~"
+  )
+
+  User.create(
+    email: "user-4@example.com",
+    password: password,
+    password_confirmation: password,
+    name: "인간미",
+    place: "덕소",
+    introduction: "호우!~"
+  )
+
+  User.create(
+    email: "user-5@example.com",
+    password: password,
+    password_confirmation: password,
+    name: "김밍밍",
+    place: "상하이",
+    introduction: "헤에?!~~"
+  )
