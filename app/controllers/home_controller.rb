a3ad50265bbfef207cad780dab745500366b149b
Ryu Hyo Kwang <hkryu0719@gmail.com>
2017-08-05 12:17:10 +0900

    홈/인덱스 화면에 포스트들 불러오기 완료

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,19 @@
 class HomeController < ApplicationController
+ 
+ 
+
 
   def index
+    @posts = @user.present? ? @user.posts.all : Post.all
+    @a_user = current_user
+
     session[:conversations] ||= []
 
     @users = User.all.where.not(id: current_user)
     @conversations = Conversation.includes(:recipient, :messages)
                                  .find(session[:conversations])
+
+
   end
 
    def photographer
@@ -15,4 +23,11 @@ class HomeController < ApplicationController
    def model
    end
 
+
+
+  
+
 end
+
+
+
