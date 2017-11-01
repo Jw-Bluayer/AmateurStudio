88e1dfd0d73b713e0865a640c6be06f13b0d7ffe
hyobbu <hyobbu@users.noreply.github.com>
2017-11-01 19:44:54 +0900

    Update 20171031153858_create_posts_tags.rb

--- db/migrate/20171031153858_create_posts_tags.rb
+++ db/migrate/20171031153858_create_posts_tags.rb
@@ -1,6 +1,6 @@
 class CreatePostsTags < ActiveRecord::Migration[5.1]
   def change
-    create_table :posts_tags, id => false do |t|
+    create_table :posts_tags do |t|
       t.references :post, foreign_key: true
       t.references :tag, foreign_key: true
     end
