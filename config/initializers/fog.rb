da50611ca1694950f5ceca87d5a75304d4516760
bluayer <ijacsong98@gmail.com>
2018-02-02 15:46:24 +0900

    image upload using carrier wave complete

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,10 +1,11 @@
+require 'carrierwave/orm/activerecord'
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
       provider:              'AWS',                        # required
-      aws_access_key_id:     'AKIAIPUSP74AHBTI6HAA',                        # required
-      aws_secret_access_key: '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',                        # required
+      aws_access_key_id:     ENV['AWS_KEY'],                        # required
+      aws_secret_access_key: ENV['AWS_SECRET'],                        # required
       region:                'ap-northeast-2',             # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'amateurstudio'            # required
+  config.fog_directory  = 'amastudio'            # required
 end
